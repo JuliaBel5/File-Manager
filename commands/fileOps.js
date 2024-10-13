fc551c6e4e6f528d7dfa67ed56b2a588f8c0c1b8
Julia Chekan <juliabel5@gmail.com>
2024-10-13 22:06:36 +0300

    fix: fix ls and cd commands

--- commands/fileOps.js
+++ commands/fileOps.js
@@ -6,30 +6,58 @@ export const up = (currentDir) => {
   return parentDir !== currentDir ? parentDir : currentDir;
 };
 
-export const cd = (currentDir, dir) => {
-  const newPath = path.resolve(currentDir, dir);
-  if (fs.existsSync(newPath) && fs.lstatSync(newPath).isDirectory()) {
-    return newPath;
+export const cd = (currentDir, targetDir) => {
+  let newDir;
+
+  if (path.isAbsolute(targetDir)) {
+    newDir = targetDir;
   } else {
-    console.log("Invalid directory");
+    newDir = path.join(currentDir, targetDir);
+  }
+
+  try {
+    if (fs.existsSync(newDir) && fs.lstatSync(newDir).isDirectory()) {
+      return newDir;
+    } else {
+      console.log("Operation failed: directory does not exist");
+      return currentDir;
+    }
+  } catch (error) {
+    console.log("Operation failed:", error.message);
     return currentDir;
   }
 };
 
 export const ls = (currentDir) => {
   try {
-    const files = fs.readdirSync(currentDir).sort();
+    const files = fs.readdirSync(currentDir);
+
+    const directories = [];
+    const regularFiles = [];
+
     files.forEach((file) => {
       try {
         const fullPath = path.join(currentDir, file);
         const type = fs.lstatSync(fullPath).isDirectory()
           ? "directory"
           : "file";
-        console.log(`${type}: ${file}`);
+
+        if (type === "directory") {
+          directories.push({ Name: file, Type: type });
+        } else {
+          regularFiles.push({ Name: file, Type: type });
+        }
       } catch (err) {
         console.error(`Operation failed for ${file}: ${err.message}`);
       }
     });
+
+    directories.sort((a, b) => a.Name.localeCompare(b.Name));
+    regularFiles.sort((a, b) => a.Name.localeCompare(b.Name));
+
+    const tableData = [...directories, ...regularFiles];
+
+    console.table(tableData);
   } catch (err) {
     console.error(`Operation failed: ${err.message}`);
   }
